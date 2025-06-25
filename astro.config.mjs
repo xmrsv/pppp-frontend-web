f0b4a7399fe8731b6467795ff8956dea6cd931f2
xmrsv <88881293+xmrsv@users.noreply.github.com>
2025-06-25 00:55:27 -0500

    Add @astrojs/vercel dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
-    output: 'server',
+  output: 'server',
+  adapter: vercel(),
 });
